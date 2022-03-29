9420221c4612f85b4a0e2c3c009b6d485d776409
cj <19415864+chinthujan@users.noreply.github.com>
2022-03-29 14:30:16 -0400

    javascript fixes

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 const tagsEl = document.getElementById('tags')
 const textarea = document.getElementById('textarea')
 
+// The focus function is used to have the page start off in the textarea box (so the cursor will start there without having to be clicked/prompted)
 textarea.focus()
 
 textarea.addEventListener('keyup', (e) => {
     createTags(e.target.value)
+
+    if(e.key === 'Enter') {
+        setTimeout(() => {
+            e.target.value = ''
+        }, 10)
+        randomSelect()
+
+    }
 })
 
 function createTags(input) {
-    const tags = input.spit(','.filter(tag => tag.trim() !== '').map(tag => tag.trim()))
+    // This line takes tags inputted by the user and logs them in a way where every time a comma is inputted, it creates a new item within the list
+    // The trim function detects any excess whitespace within a list item and cuts it out (example: "2 " would be logged as "2")
+    const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
     
     tagsEl.innerHTML = ''
 
@@ -16,6 +27,48 @@ function createTags(input) {
         const tagEl = document.createElement('span')
         tagEl.classList.add('tag')
         tagEl.innerText = tag
-        tagsEl.appendChild(tagsEl)
+        tagsEl.appendChild(tagEl)
     })
+}
+
+function randomSelect() {
+    const times = 30
+
+    // The following lines are responsible for animating the selecting of each randomized item within the list
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag()
+
+        highlightTag(randomTag)
+
+        setTimeout(() => {
+            unHighlightTag(randomTag)
+        }, 100)
+    }, 100);
+
+    // The following lines are responsible for stopping the animation from above and selecting/stopping the selection process on one random item
+    setTimeout(() => {
+        clearInterval(interval)
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag ()
+
+            highlightTag(randomTag)
+        }, 100)
+    }, times * 100)
+}
+
+// Creates a list of all of the tags that were entered, randomizes one, uses math.floor to round down -- this function is used to pick one of the inputted tags at random
+function pickRandomTag() {
+    const tags = document.querySelectorAll('.tag')
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+// Adds a highlight to one of the user-entered list items
+function highlightTag(tag) {
+    tag.classList.add('highlight')
+}
+
+// Removes the highlight from the front end list
+function unHighlightTag(tag) {
+    tag.classList.remove('highlight')
 }
